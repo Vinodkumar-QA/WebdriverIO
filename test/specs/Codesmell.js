52f0998df5d3e50be2f16654e00b99135e6417a9
vinod <vinodkumarbellamkonda@gmail.com>
2024-02-12 15:06:19 +0530

    code smell test

---
+++ test/specs/Codesmell.js
@@ -0,0 +1,18 @@
+function calculateTotalPrice(item) {
+    // Code to calculate the price
+    // ...
+    return totalPrice;
+  }
+  
+  function calculateTax(item) {
+    // Code to calculate tax
+    // ...
+    return taxAmount;
+  }
+  
+  function calculateFinalPrice(item) {
+    // Code to calculate the final price
+    // ...
+    return finalPrice;
+  }
+  
